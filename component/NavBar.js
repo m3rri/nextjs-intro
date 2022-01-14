e4f259c913638e663bd76ed4b03dd8738ac94363
kim hye ri <hrkim2@seegene.com>
2022-01-14 17:28:02 +0900

    ~styled jsx

--- component/NavBar.js
+++ component/NavBar.js
@@ -1,19 +1,28 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
+import css from 'styled-jsx/css';
 
+const style = css`
+nav {
+    background-color: tomato;
+}
+a {
+    text-decoration: none;
+}
+.active {
+    color: yellow;
+}
+`;
 export default function NavBar(){
     const router = useRouter();
 
     return <nav>
         <Link href="/">
-            <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
+            <a className={router.pathname==='/' ? 'active': ''}>Home</a>
         </Link>
         <Link href="/about">
-            <a className={[
-                styles.link,
-                router.pathname === '/about' ? styles.active : ''
-            ].join(' ')}>About</a>
+            <a className={router.pathname==='/about' ? 'active': ''}>About</a>
         </Link>
+        <style jsx>{style}</style> {/** html tag */}
     </nav>
 }
