d2a60ba224506f7f52a5a760a705cd72c90e92e7
kim hye ri <hrkim2@seegene.com>
2022-01-17 11:16:48 +0900

    ~Fetching Data

--- component/NavBar.js
+++ component/NavBar.js
@@ -2,27 +2,76 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import css from 'styled-jsx/css';
 
-const style = css`
-nav {
-    background-color: tomato;
-}
-a {
-    text-decoration: none;
-}
-.active {
-    color: yellow;
-}
-`;
+// const style = css`
+//   a {
+//     text-decoration: none;
+//   nav {
+//     display: flex;
+//     gap: 10px;
+//     flex-direction: column;
+//     align-items: center;
+//     padding-top: 20px;
+//     padding-bottom: 10px;
+//     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+//       rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+//   }
+//   img {
+//     max-width: 100px;
+//     margin-bottom: 5px;
+//   }
+//   nav a {
+//     font-weight: 600;
+//     font-size: 18px;
+//   }
+//   .active {
+//     color: tomato;
+//   }
+//   nav div {
+//     display: flex;
+//     gap: 10px;
+//   }
+// `;
 export default function NavBar(){
     const router = useRouter();
 
-    return <nav>
-        <Link href="/">
-            <a className={router.pathname==='/' ? 'active': ''}>Home</a>
-        </Link>
-        <Link href="/about">
-            <a className={router.pathname==='/about' ? 'active': ''}>About</a>
-        </Link>
-        <style jsx>{style}</style> {/** html tag */}
-    </nav>
+    return (
+    <nav>
+        <img src="/vercel.svg" />
+        {/* public에 있는 리소스의 경우 ../ , ./ 이런식으로 경로 안해도 됨 */}
+        <div>
+            <Link href="/">
+                <a className={router.pathname==='/' ? 'active': ''}>Home</a>
+            </Link>
+            <Link href="/about">
+                <a className={router.pathname==='/about' ? 'active': ''}>About</a>
+            </Link>
+        </div>
+        <style jsx>{`
+            nav {
+                display: flex;
+                gap: 10px;
+                flex-direction: column;
+                align-items: center;
+                padding-top: 20px;
+                padding-bottom: 10px;
+                box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+                rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+            }
+            img {
+                max-width: 100px;
+                margin-bottom: 5px;
+            }
+            nav a {
+                font-weight: 600;
+                font-size: 18px;
+            }
+            .active {
+                color: tomato;
+            }
+            nav div {
+                display: flex;
+                gap: 10px;
+            }
+        `}</style> {/** html tag */}
+    </nav>);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Seo from '../component/Seo';
 
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+//노출되어 있기 때문에 감춰야하는 필요성이 생김
+
 export default function Home(){
+    const [movies, setMovies] = useState();
+    useEffect(() => {
+      (async () => {
+        const { results } = await (
+          await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+          )
+        ).json();
+        
+        setMovies(results);
+      })();
+    }, []);
+
     return (
         <div>
             <Seo title='Home'/>
-            <h1>hi</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </div>
     )
 }
