cccdc8adbe715e1290fcac55b4c733b19e9edd39
kim hye ri <hrkim2@seegene.com>
2022-01-18 10:57:33 +0900

    ~Movie Detail

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,8 +2,12 @@ import {useRouter} from 'next/router';
 
 export default function Detail(){
     const router = useRouter();
-    const {id} = router.query;
+    const {id, title} = router.query;
     console.log(router);
     console.log(`id : ${id}`);
-    return 'detail here';
+    console.log(`title : ${title}`);
+    
+    return <div>
+        <h4>{title || "loading..."}</h4>
+    </div>;
 }
